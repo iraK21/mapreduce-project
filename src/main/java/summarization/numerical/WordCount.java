8b3e320d4bcbf8c221ae47a3584caa639fc2c903
iraK21 <irakawthalkar21@gmail.com>
2018-07-05 15:49:58 +0530

    Updated Word Count

--- src/main/java/summarization/numerical/WordCount.java
+++ src/main/java/summarization/numerical/WordCount.java
@@ -16,20 +16,11 @@ import java.util.StringTokenizer;
  */
 public class WordCount {
 
-    /**
-     * Mapper class for WordCount.
-     * It takes in a line of text and maps it to a count of 1.
-     */
     public static class WordCountMapper
             extends Mapper<Object, Text, Text, IntWritable> {
 
-        /** This field serves as a placeholder for every word. */
         private Text word = new Text();
 
-        /**
-         * This is called once for each (line - text-in-line) pair of the input.
-         * It parses the XML into a HashMap, then tokenizes the comment text and maps every word to a value of 1
-         */
         public void map(Object key,
                         Text value,
                         Context context)
@@ -48,19 +39,10 @@ public class WordCount {
         }
     }
 
-    /**
-     * Reducer class for Word Count.
-     * This class aggregates all the counts corresponding to a given key (word), and writes the sum to the output.
-     * Note that this class can also be used as a combiner.
-     */
     public static class WordCountReducer
             extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
-        /**
-         * This method iterates through all values corresponding to a given key.
-         * It thens sums them up to give the final word count, and writes the count to output.
-         */
         public void reduce(Text key,
                            Iterable<IntWritable> values,
                            Context context)
